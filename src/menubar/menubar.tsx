a00239648e5a9c7f8b3051a2553f73d09464bff6
Anders Jefsen <andersjeff397@gmail.com>
2024-02-23 11:11:44 +0100

    routing set up + base landing page created

--- src/menubar/menubar.tsx
+++ src/menubar/menubar.tsx
@@ -3,26 +3,26 @@ import './menubar.css';
 import search from '../assets/search.svg'
 import basket from '../assets/basket.svg'
 import cofee from '../assets/coffee.svg'
-
+import Link from '../components/Link';
 
 export function Menubar() {
     return (
         <div className="menubar-container">
-            <img src={cofee} alt="coffee" className="coffee-icon" />
+                <Link to="/"> <img src={cofee} alt="coffee"/></Link> 
             <div className="menu-items">
-                <a href="#" className="menu-item">DINNERWARE</a>
-                <a href="#" className="menu-item">DRINKWARE</a>
-                <a href="#" className="menu-item">SERVEWARE</a>
-                <a href="#" className="menu-item">TABLE ACCESSORIES</a>
-                <a href="#" className="menu-item">ALL PRODUCTS</a>
+                <Link to="/dinnerware" className='menu-item'>DINNERWARE</Link>
+                <Link to="/drinkware"  className='menu-item'>DRINKWARE</Link>
+                <Link to="/serveware"  className='menu-item'>SERVEWARE</Link>
+                <Link to="/table-accessories" className='menu-item'>TABLE ACCESSORIES</Link>
+                <Link to="/all-products" className='menu-item'>ALL PRODUCTS</Link>
             </div>
-            <div className="menu-icons">
-                <a href="#" target="_blank" rel="noreferrer noopener">
-                    <img src={search} alt="Search icon" className="menu-icon" />
-                </a>
-                <a href="#" target="_blank" rel="noreferrer noopener">
+            <div>
+                <Link to="#" target="_self" rel="noreferrer noopener">
+                        <img src={search} alt="Search icon" className="menu-icon" />
+                    </Link>
+                <Link to="/cart" target="_self" rel="noreferrer noopener">
                     <img src={basket} alt="basket icon" className="menu-icon" />
-                </a>
+                </Link>
             </div>
         </div>
     );
