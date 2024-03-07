341336ac2b710c36569729ff4dac35aac5605a05
Jeanyagan <72025250+Jeanyagan@users.noreply.github.com>
2024-03-07 23:08:30 +0100

    Tilføjet 2 kolonner til checkout. Nu klar til at få tilføjet indtastningsfelter til shipping info

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+// import React, { useState } from "react";
+import './Checkout.css' // Stien til CSS-fil
+
 
 const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout Page</h1>
-      {/* Tilføj yderligere markup og funktionalitet her */}
+    <div className="checkout-page">
+      <div className="checkout-form">
+        <h2>Kontaktoplysninger</h2>
+        {/* Indsætter formular for kontaktoplysninger */}
+        
+        <h2>Levering</h2>
+        {/* Formular for leveringsoplysninger */}
+      </div>
+
+      <div className="order-summary">
+        <h2>Din ordre</h2>
+        {/* Elementer for ordresammen drag */}
+      </div>
     </div>
   );
 };
