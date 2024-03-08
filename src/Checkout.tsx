d8c557337add0d50095b5b36b6261866eb6f643b
Jeanyagan <72025250+Jeanyagan@users.noreply.github.com>
2024-03-08 12:01:31 +0100

    Tilføjet felterne for email, Fornavn, Efternavn og checkbox for nyhedsbrev. Alle celler er intagerebare, dog checker de endnu ikke for korrekthed i f.eks. emailen

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,21 +1,43 @@
-// import React, { useState } from "react";
 import './Checkout.css' // Stien til CSS-fil
-
 
 const Checkout = () => {
   return (
     <div className="checkout-page">
-      <div className="checkout-form">
+      <div className="checkout-left">
         <h2>Kontaktoplysninger</h2>
-        {/* Indsætter formular for kontaktoplysninger */}
-        
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+              type="email"
+              id="email"
+              placeholder="Indtast din email"
+            />
+          <input
+              type="checkbox"
+              id="subscribe"
+            />
+          <label htmlFor="subscribe">Send mails til mig om nyheder og tilbud</label>
+        </div>
         <h2>Levering</h2>
-        {/* Formular for leveringsoplysninger */}
+        <div className="input-group"> 
+          <label htmlFor="country">Land/område</label>
+          <select id="country">
+            <option value="denmark">Danmark</option>
+          </select>
+        </div>
+        <div className="input-group">
+          <label htmlFor="firstname">Fornavn</label>
+          <input type="text" id="firstname" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="lastname">Efternavn</label>
+          <input type="text" id="lastname" />
+        </div>
+        {/* Add more input fieds here */}
       </div>
-
       <div className="order-summary">
         <h2>Din ordre</h2>
-        {/* Elementer for ordresammen drag */}
+        {/* Elements for order summary */}
       </div>
     </div>
   );
