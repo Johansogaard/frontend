d3d08e849cbf8bf3fc79a956eabffc74cd80efef
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2024-03-10 13:47:11 +0100

    added first test

---
+++ tests/components/shopping-cart.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import {ShoppingCart} from "../../src/shopping-cart/shoppingCart";
+
+describe('ShoppingCart', () => {
+  it("should render the shopping cart", () => {
+    render(<ShoppingCart />);
+    expect(screen.getByText("Contact Information")).toBeDefined();
+  });
+});
