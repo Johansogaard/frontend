127711d71050ff4087c6826c10b77e6a64638c1f
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2024-03-10 11:29:22 +0100

    testing pushing an error

--- src/shopping-cart/shoppingCart.tsx
+++ src/shopping-cart/shoppingCart.tsx
@@ -130,7 +130,7 @@ export function ShoppingCart() {
 }
 
 function shoppingCartItem(item: Item, 
-    handleShopQuantityComponent: (itemId: number, change: number) => void,
+    handleShopQuantityComponent: (itemId, change: number) => void,
     removeItem: (itemId: number) => void
     ) {
     return (
