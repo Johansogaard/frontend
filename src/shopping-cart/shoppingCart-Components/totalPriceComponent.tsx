a14dc0f537f2fc25be1db899af012285c38cf8b5
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2024-04-06 12:30:58 +0200

    ui

--- src/shopping-cart/shoppingCart-Components/totalPriceComponent.tsx
+++ src/shopping-cart/shoppingCart-Components/totalPriceComponent.tsx
@@ -13,24 +13,24 @@ return(
         <hr></hr>
         {items.map((item) => (
           <div key={item.product.product_id} className="item-subtotal">
-            <h3>
-              {item.product.product_name} Subtotal: {calcItemSubTotal(item.product.category_id)} DKK
-            </h3>
-            <p>{remainingItemsForDiscount(item.product.product_id)}</p>
+            <div className='item-subtotal-price'>
+            <span>
+              {item.product.product_name} x {item.quantity} 
+            </span>
+            <span>{calcItemSubTotal(item.product.product_id)}</span>
+            </div>
+            <div className='item-subtotal-discount'>
+            <span>{remainingItemsForDiscount(item.product.product_id)}</span>
+            </div>
           </div>
 
           //This Div aligns the subtotal with the item.id and it's row
         ))}
 
         <div className="subtotal-total">
-          <h2>Total Items</h2>
-          <h3>{calcTotalItems()} </h3>
-          <h2>Total</h2>
-          <h3>
-            {parseFloat(calcTotal()) > 300
-              ? '10% Discount Applied!'
-              : `Add ${(300 - parseFloat(calcTotal())).toFixed(2)} more to get a 10% discount`}
-          </h3>
+          <strong>Your total price</strong>
+          <strong>{calcTotal()}</strong> 
+        
           
         </div>
         <button className="checkout-button">
