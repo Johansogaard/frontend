e32d398356b0eaf583408dbff3e752c87f54e2b8
Anders Jefsen <andersjeff397@gmail.com>
2024-04-15 14:59:24 +0200

    Refactor routing to use react-router-dom and add handleCheckout function for payment initiation

--- src/shopping-cart/shoppingCart-Components/handleCheckout.tsx
+++ src/shopping-cart/shoppingCart-Components/handleCheckout.tsx
@@ -1,18 +1,22 @@
 import { Item } from "../../models/Item";
 import { loadStripe } from '@stripe/stripe-js';
-export async function handleCheckout(items: Item[]) {
-    try {
-      console.log('Items:', items);
-  
-      const bodyData = {
-        items: items,
-      };
+export async function handleCheckout(items: Item[], customer_id: string, shipping_address: string, billing_address: string, total_amount: number) {
+  try {
+    const bodyData = {
+      items: items,
+      metadata: {
+        customer_id: customer_id,
+        shipping_address: shipping_address,
+        billing_address: billing_address,
+        total_amount: total_amount,
+      },
+    };
   
       console.log('Body Data:', bodyData);
   
       // make a POST request to server to create a checkout session
-    //const response = await fetch('http://localhost/payments/create-checkout-session', {
-    const response = await fetch(`https://dtu62597.eduhost.dk:10132/payments/create-checkout-session`, {
+    const response = await fetch('https://localhost/payments/create-checkout-session', {
+    //const response = await fetch(`https://dtu62597.eduhost.dk:10132/payments/create-checkout-session`, {
             
         method: 'POST',
         headers: {
@@ -26,7 +30,7 @@ export async function handleCheckout(items: Item[]) {
       // redirect user to checkout page
       const stripe = await loadStripe('pk_test_51OiCXmBMSCX1jgl0IUNdbTl3vYHRLFCzO2Hf6HsfmbdZGFvy3lwCWkYYAOaLDRXaT6XvqiT0hlnJOJ4cMqmcWBTS00TwtT3JgH');
       if (stripe) {
-        stripe.redirectToCheckout({ sessionId });
+        stripe.redirectToCheckout({ sessionId});
       }
     } catch (error) {
       console.error('Error during checkout:', error);
